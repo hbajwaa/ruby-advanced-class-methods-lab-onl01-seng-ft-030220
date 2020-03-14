d829e7b56a3da54f68078a7ab313434b8786bc8c
Hassan Bajwa <bajwahassan3@outlook.com>
2020-03-14 11:16:49 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -16,9 +16,9 @@ class Song
     song
   end
   
-  def self.new_by_name(name)
+  def self.new_by_name(name_song)
     song = self.new 
-    song.name = name
+    song.name = name_song
     song
   end
   
