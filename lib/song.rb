bf41ae9a9e8a9ed7980b78d2e07ce07bec3c8b62
Hassan Bajwa <bajwahassan3@outlook.com>
2020-03-14 11:34:20 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -36,9 +36,7 @@ class Song
   end
   
   def self.find_or_create_by_name(song_name)
-    @@all.each do |song|
-      song.name == song_name
-    end
+    self.find_by_name(song_name) || self.create_by_name(song_name)
   end
   
   
